1ad48a606753a373eebc97a0b6e143e5ac51c7f0
minhthai <minhthai23012002@gmail.com>
2024-05-12 09:54:56 +0700

    first commit

---
+++ utils/constant.ts
@@ -0,0 +1,14 @@
+export const configs = {
+    // baseURL: "https://subenbackend.onrender.com/"
+    baseURL: "http://localhost:5000",
+    // baseURL : "https://server-iccq.onrender.com"
+}
+
+export const colors = {
+    main: "#1877F2"
+}
+
+export const GIFJSON = {
+    Welcome: require("../asset/Json/welcome.json"),
+    Typing : require("../asset/Json/typing.json")
+}
